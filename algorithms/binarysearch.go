f9dfce6f211f72095c9593a4dfd271fa220c4215
Gill Erick <ogayogill95@gmail.com>
2023-02-03 14:26:45 +0300

    Add BinarySearch tests

--- algorithms/binarysearch.go
+++ algorithms/binarysearch.go
@@ -1,5 +1,7 @@
 package main
 
+import "testing"
+
 func BinarySearch(array []int, target int) int {
 	leftIdx := 0
 	rightIdx := len(array)
@@ -12,10 +14,28 @@ func BinarySearch(array []int, target int) int {
 			return potentialTarget
 		} else if potentialTarget > target {
 			rightIdx = mid - 1
-
 		} else if potentialTarget < target {
 			leftIdx = mid + 1
 		}
 	}
 	return -1
+}
+
+func TestBinarySearch(t *testing.T) {
+	tests := []struct {
+		array  []int
+		target int
+		result int
+	}{
+		{[]int{0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 33, 3},
+		{[]int{1, 5, 23, 111}, 111, 3},
+		{[]int{1, 5, 23, 111}, 5, 1},
+		{[]int{1, 5, 23, 111}, 35, -1},
+		{[]int{0, 1, 21, 33, 45, 45, 61, 71, 72, 73}, 0, 0},
+	}
+	for _, test := range tests {
+		if got := BinarySearch(test.array, test.target); got != test.result {
+			t.Errorf("binarySearch(%v, %d) = %d; expected %d", test.array, test.target, got, test.result)
+		}
+	}
 }
